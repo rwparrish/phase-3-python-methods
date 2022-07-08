061b311f0d2fb620c4473f506dbde31e57d79609
rwparrish <60946750+rwparrish@users.noreply.github.com>
2022-07-08 10:34:37 -0600

    one left

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
+from tkinter.tix import INTEGER
+
+
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
 
 def greet(name):
-    pass
+    print(f'Hello, {name}!')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if type(number) != INTEGER:
+        return None
+    else:
+        return number/2
